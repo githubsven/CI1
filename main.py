3c6331e48e187ad6124e4e780a91b095c1e13c8a
Sven Luehof <svenluehof@Svens-MacBook-Pro.local>
2017-05-16 15:00:38 +0200

    Checks toegevoegd voor of een cijfer al in een rij/kolom zit

--- main.py
+++ main.py
@@ -12,26 +12,21 @@ def getSudoku(fileName):
 
     return sudoku
 
-def backtracking():
-    #once: findFirstEmpySpot
-    #keep track of row and column numbers (so you know which number you're changing)
-    if empty(L) then return nil
-    else
-        t ← first(L);
-    if goal(t) then return t
-    else
-        while there are unexplored successors of t
-    and not found
-    do
-    t’ ← next - successor(t);
-    L ← push(L, t’);
-    backtrack(L)
-    endwhile;
-    L ← pop(L)
+def inRow(sudoku, rowNumber, number):
+    for i in range(len(sudoku[0])):
+        if sudoku[rowNumber][i] == number:
+            return True
+    return False
 
-def nextSuccessor(rowNumber, columnNuber, sudoku):
-    #check if valid successor
-    return sudoku
+def inColumn(sudoku, columnNumber, number):
+    for i in range(len(sudoku)):
+        if sudoku[i][columnNumber] == number:
+            return True
+    return False
+
+def inBlock(sudoku, rowNumber, number):
+
+    return False
 
 def findFirstEmptySpot(sudoku):
     for x in range(len(sudoku[0])):
@@ -39,6 +34,17 @@ def findFirstEmptySpot(sudoku):
             if sudoku[x][y] == 0:
                 return x, y
     return None
+
+def nextSuccessor(rowNumber, columnNumber, sudoku):
+    #check if valid successor
+    return sudoku
+
+def backtracking(sudoku):
+    rowNumber, columnNumber = findFirstEmptySpot(sudoku)
+
+    #once: findFirstEmpySpot
+    #keep track of row and column numbers (so you know which number you're changing)
+    return sudoku
 
 if __name__ == '__main__':
     print getSudoku("sudoku.txt")
