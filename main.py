c59ad9e645b298fa1cf5038cca9629df670d2e39
Sven Luehof <svenluehof@Svens-MacBook-Pro.local>
2017-05-18 10:49:35 +0200

    Sudoku kan nu worden opgelost op de normale doorloop manier en op de reverse doorloop manier

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 def getSudoku(fileName):
     f = open(fileName, "r")
     rows = f.read().split("\n")
@@ -8,7 +10,7 @@ def getSudoku(fileName):
     for index in range(len(rows)):
         columns = rows[index].split()
         for counter, column in enumerate(columns):
-            sudoku[index][counter] = columns[counter]
+            sudoku[index][counter] = (int)(columns[counter])
 
     return sudoku
 
@@ -24,27 +26,85 @@ def inColumn(sudoku, columnNumber, number):
             return True
     return False
 
-def inBlock(sudoku, rowNumber, number):
+def inBlock(sudoku, rowNumber, columnNumber, number):
+    length = int(math.sqrt(len(sudoku)))
+    blockRow = rowNumber - rowNumber % length
+    blockColumn = columnNumber - columnNumber % length
+    for x in range(length):
+        for y in range(length):
+            if sudoku[blockRow + x][blockColumn + y] == number:
+                return True
+    return False
+
+def acceptNumber(sudoku, rowNumber, columnNumber, number):
+    return not inRow(sudoku, rowNumber, number) and not inColumn(sudoku, columnNumber, number) and not inBlock(sudoku, rowNumber, columnNumber, number)
+
+def findFirstEmptySpot(sudoku):
+    for x in range(len(sudoku)):
+        for y in range(len(sudoku[0])):
+            if sudoku[x][y] == 0:
+                return x, y, True
+    return 0, 0, False
+
+def findFirstEmptySpotReverse(sudoku):
+    for x in range(len(sudoku))[::-1]:
+        for y in range(len(sudoku[0]))[::-1]:
+            if sudoku[x][y] == 0:
+                return x, y, True
+    return 0, 0, False
+
+def prettyPrint(sudoku):
+    length = len(sudoku)
+    result = ""
+    for x in range(length):
+        for y in range(length):
+            result += str(sudoku[x][y]) + " "
+        result += "\n"
+
+    return result
+
+def backtracking(sudoku):
+
+    rowNumber, columnNumber, found = findFirstEmptySpot(sudoku)
+
+    print prettyPrint(sudoku)
+
+    if not found:
+        return True
+
+    for number in range(1, 10):
+        if acceptNumber(sudoku, rowNumber, columnNumber, number):
+            sudoku[rowNumber][columnNumber] = number
+
+            if backtracking(sudoku):
+                return True
+
+            sudoku[rowNumber][columnNumber] = 0
 
     return False
 
-def findFirstEmptySpot(sudoku):
-    for x in range(len(sudoku[0])):
-        for y in range(len(sudoku)):
-            if sudoku[x][y] == 0:
-                return x, y
-    return None
+def reverseBacktracking(sudoku):
+    rowNumber, columnNumber, found = findFirstEmptySpotReverse(sudoku)
 
-def nextSuccessor(rowNumber, columnNumber, sudoku):
-    #check if valid successor
-    return sudoku
+    print prettyPrint(sudoku)
 
-def backtracking(sudoku):
-    rowNumber, columnNumber = findFirstEmptySpot(sudoku)
+    if not found:
+        return True
 
-    #once: findFirstEmpySpot
-    #keep track of row and column numbers (so you know which number you're changing)
-    return sudoku
+    for number in range(1, 10):
+        if acceptNumber(sudoku, rowNumber, columnNumber, number):
+            sudoku[rowNumber][columnNumber] = number
+
+            if reverseBacktracking(sudoku):
+                return True
+
+            sudoku[rowNumber][columnNumber] = 0
+
+    return False
 
 if __name__ == '__main__':
-    print getSudoku("sudoku.txt")
+    sudoku = getSudoku("sudoku.txt")
+    if reverseBacktracking(sudoku):
+        print sudoku
+    else:
+        print "Geen oplossing"
