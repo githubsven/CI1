c9d7aec5951dac12f62e9fb4255c970746db1cb6
Sven Luehof <s.luehof@gmail.com>
2017-06-10 20:13:37 +0200

    Verder met deel 2

--- main.py
+++ main.py
@@ -5,13 +5,30 @@ def getSudoku(fileName):
     rows = f.read().split("\n")
 
     width, height = len(rows[0].split()), len(rows)
-    emptySquare = Square()
-    sudoku = [[emptySquare for x in range(width)] for y in range(height)]
+    sudoku = [[0 for x in range(width)] for y in range(height)]
 
-    for index in range(len(rows)):
+    for index in range(height):
         columns = rows[index].split()
         for counter, column in enumerate(columns):
-            sudoku[index][counter] = sudoku[index][counter].value = (int)(columns[counter])
+            sudoku[index][counter] = columns[counter]
+
+    return sudoku
+
+def getSudokuWithSquares(fileName):
+    f = open(fileName, "r")
+    rows = f.read().split("\n")
+
+    width, height = len(rows[0].split()), len(rows)
+    sudoku = [[Square() for x in range(width)] for y in range(height)]
+
+    for index in range(height):
+        columns = rows[index].split()
+        for counter, column in enumerate(columns):
+            sudoku[index][counter].value = (int)(columns[counter])
+
+    for row in range(height):
+        for column in range(width):
+            sudoku[row][column].fillDomain(sudoku, row, column)
 
     return sudoku
 
@@ -154,35 +171,121 @@ def getDomainSize(sudoku, rowNumber, columnNumber):
 
     return len(sudoku) - len(domain)
 
+def inRowSquare(sudoku, rowNumber, number):
+    for i in range(len(sudoku[0])):
+        if sudoku[rowNumber][i].value == number:
+            return True
+    return False
+
+def inColumnSquare(sudoku, columnNumber, number):
+    for i in range(len(sudoku)):
+        if sudoku[i][columnNumber].value == number:
+            return True
+    return False
+
+def inBlockSquare(sudoku, rowNumber, columnNumber, number):
+    length = int(math.sqrt(len(sudoku)))
+    blockRow = rowNumber - rowNumber % length
+    blockColumn = columnNumber - columnNumber % length
+    for x in range(length):
+        for y in range(length):
+            if sudoku[blockRow + x][blockColumn + y].value == number:
+                return True
+    return False
+
+def acceptNumberSquare(sudoku, rowNumber, columnNumber, number):
+    return not inRowSquare(sudoku, rowNumber, number) and not inColumnSquare(sudoku, columnNumber, number) and not inBlockSquare(sudoku, rowNumber, columnNumber, number)
+
+
+def forwardChecking(sudoku, sortedMCVList, counter, index = 0):
+    counter.up()
+    if index < len(sortedMCVList):
+        rowNumber, columnNumber, domainSize = sortedMCVList[index]
+    else:
+        return True, counter.count()
+
+    for number in sudoku[rowNumber][columnNumber].getDomain():
+        sudoku[rowNumber][columnNumber].value = number
+        index += 1
+
+        if domainSizeBacktracking(sudoku, sortedMCVList, counter, index):
+            return True, counter.count()
+
+        sudoku[rowNumber][columnNumber] = 0
+        index -= 1
+
+    return False
+
+def getSortedMCVList(sudoku):
+    l = []
+    for x in range(len(sudoku)):
+        for y in range(len(sudoku)):
+            if sudoku[x][y].value == 0:
+                l.append((x, y, len(sudoku[x][y].getDomain())))
+
+    return sorted(l, key=lambda x: x[2])
 
 class Square:
     value = 0
-    domain = []
+    domain = set()
+
+    def getDomain(self):
+        return list(self.domain)
 
     def fillDomain(self, sudoku, rowNumber, columnNumber):
         sudokuSize = len(sudoku)
-        domainSet = set()
+        self.domain = set(i + 1 for i in range(sudokuSize))
 
         for x in range(sudokuSize):
-            domainSet.add(sudoku[rowNumber][x])
+            self.domain.discard(sudoku[rowNumber][x].value)
 
         for y in range(sudokuSize):
-            domainSet.add(sudoku[y][columnNumber])
+            self.domain.discard(sudoku[y][columnNumber].value)
 
-        blockLength = int(math.sqrt(len(sudoku)))
+        blockLength = int(math.sqrt(sudokuSize))
         blockRow = rowNumber - rowNumber % blockLength
         blockColumn = columnNumber - columnNumber % blockLength
         for x in range(blockLength):
             for y in range(blockLength):
-                domainSet.add(sudoku[blockRow + x][blockColumn + y])
+                self.domain.discard(sudoku[blockRow + x][blockColumn + y].value)
 
-        self.domain = list(domainSet)
+    def removeValueFromNeighboursDomain(self, sudoku, rowNumber, columnNumber):
+        sudokuSize = len(sudoku)
+
+        for x in range(sudokuSize):
+            sudoku[rowNumber][x].domain.discard(self.value)
+
+        for y in range(sudokuSize):
+            sudoku[y][columnNumber].domain.discard(self.value)
+
+        blockLength = int(math.sqrt(sudokuSize))
+        blockRow = rowNumber - rowNumber % blockLength
+        blockColumn = columnNumber - columnNumber % blockLength
+        for x in range(blockLength):
+            for y in range(blockLength):
+                sudoku[blockRow + x][blockColumn + y].domain.discard(self.value)
+
+    def addValueToNeighboursDomain(self, sudoku, rowNumber, columnNumber):
+        sudokuSize = len(sudoku)
+
+        for x in range(sudokuSize):
+            sudoku[rowNumber][x].domain.add(self.value)
+
+        for y in range(sudokuSize):
+            sudoku[y][columnNumber].domain.add(self.value)
+
+        blockLength = int(math.sqrt(sudokuSize))
+        blockRow = rowNumber - rowNumber % blockLength
+        blockColumn = columnNumber - columnNumber % blockLength
+        for x in range(blockLength):
+            for y in range(blockLength):
+                sudoku[blockRow + x][blockColumn + y].domain.add(self.value)
 
     def isDomainEmpty(self):
         return len(self.domain) == 0
 
     def removeFromDomain(self, val):
-        self.domain.remove(val)
+        self.domain.discard(val)
 
 
 class Counter:
@@ -195,14 +298,17 @@ class Counter:
         return self.i
 
 if __name__ == '__main__':
-    sudoku = getSudoku("sudoku.txt")
+    sudoku = getSudokuWithSquares("sudoku.txt")
+    sortedMCVList = getSortedMCVList(sudoku)
+    print sortedMCVList
     start_time = time.time()
     counter = Counter()
 
     #sortedList = getListExpandByDomainSize(sudoku)
     #solved, recursion = domainSizeBacktracking(sudoku, sortedList, counter)
-    solved, recursion = reverseBacktracking(sudoku, counter)
+    #solved, recursion = reverseBacktracking(sudoku, counter)
     #solved, recursion = backtracking(sudoku, counter)
+    solved, recursion = forwardChecking(sudoku, sortedMCVList, counter)
     if solved:
         print sudoku
     else:
